fc3cb2981a15f66f009451d9818f43b27171154c
Ayomi Satria Pratama <72070889+Ayomi29@users.noreply.github.com>
2022-04-14 22:55:06 +0800

    Up fitur CRUD (Create, Read, Update, Delete)

--- app/Http/Controllers/DashboardPostController.php
+++ app/Http/Controllers/DashboardPostController.php
@@ -56,17 +56,38 @@ class DashboardPostController extends Controller
 
     public function edit(Post $post)
     {
-        //
+        return view('dashboard.posts.edit', [
+            'post' => $post,
+            'categories' => Category::all()
+        ]);
     }
 
     public function update(Request $request, Post $post)
     {
-        //
+        $rules_validate = [
+            'title' => 'required|max:255',
+            'category_id' => 'required',
+            'body' => 'required'
+        ];
+
+        if ($request->slug != $post->slug) {
+            $rules_validate['slug'] = 'required|unique:posts';
+        }
+
+        $validateData = $request->validate($rules_validate);
+        $validateData['user_id'] = auth()->user()->id;
+        $validateData['excerpt'] = Str::limit(strip_tags($request->body), 100);
+
+        Post::where('id', $post->id)->update($validateData);
+
+        return redirect('/dashboard/posts')->with('success', 'Posting anda telah berhasil diupdate!');
     }
 
     public function destroy(Post $post)
     {
-        //
+        Post::destroy($post->id);
+
+        return redirect('/dashboard/posts')->with('success', 'Posting anda telah berhasil dihapus!');
     }
     public function checkSlug(Request $request)
     {
--- resources/views/dashboard/posts/show.blade.php
+++ resources/views/dashboard/posts/show.blade.php
@@ -6,8 +6,12 @@
             <div class="col-lg-8">
                 <h1 class="mb-3">{{ $post->title }}</h1>
                 <a href="/dashboard/posts" class="btn btn-success"><span data-feather="arrow-left"></span> Back to my posts</a>
-                <a href="" class="btn btn-warning"><span data-feather="edit"></span> Edit</a>
-                <a href="" class="btn btn-danger"><span data-feather="x-circle"></span> Delete</a>
+                <a href="/dashboard/posts/{{ $post->slug }}/edit" class="btn btn-warning"><span data-feather="edit"></span> Edit</a>
+                <form action="/dashboard/posts/{{ $post->slug }}" method="post" class="d-inline">
+                  @method('delete')
+                  @csrf
+                  <button class="btn btn-danger" onclick="return confirm('Apakah anda yakin ingin menghapus post ini?')"><span data-feather="x-circle"></span> Delete</button>
+                </form>
                 <img src="https://picsum.photos/1200/300" class="img-fluid mt-3" alt="{{ $post->category->name }}">
                 <article class="my-3">
                     {!! $post->body !!}
